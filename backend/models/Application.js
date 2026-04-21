4b0902143a1f8eef57c74d06039c92146a099570
Sibtain Raza <khanbhai14063@gmail.com>
2026-04-21 23:26:59 +0530

    WIP: project root with backend work

---
+++ backend/models/Application.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema({
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  job: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job"
+  },
+  status: {
+    type: String,
+    enum: ["Applied", "Shortlisted", "Rejected"],
+    default: "Applied"
+  }
+});
+
+module.exports = mongoose.model("Application", applicationSchema);
